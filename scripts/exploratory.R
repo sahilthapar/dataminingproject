3331450356e04981ff345402bcfa5108987c8dcb
Sahil Thapar <sahilthapar1992@gmail.com>
2017-04-03 20:11:50 -0400

    Merged inital EDA of first 4 variables; merged store data and training data

--- scripts/exploratory.R
+++ scripts/exploratory.R
@@ -1,10 +1,20 @@
+<<<<<<< Updated upstream
 library(readr)
 library(ggplot2)
 library(lubridate)
 library(stringr)
+=======
+library(readr) # Reading csv files
+library(tidyr) # Data manipulation
+library(dplyr) # Data manipulation
+library(ggplot2) # Plots
+library(forcats) # Factors
+
 ## Note setup:
-#Add your data folder into this directory and
+# Add your data folder into this directory and
+
 # Read files
+
 rossman.train <- read_csv(file = './data/train.csv')
 rossman.test <- read_csv(file = './data/test.csv')
 rossman.store <- read_csv(file = './data/store.csv')
@@ -26,17 +36,60 @@ table(rossman.store$StoreType)
 table(rossman.store$Promo2)
 table(rossman.store$PromoInterval)
 
-#Create a new variable CompetitionStart in date format combining 
-#CompetitionOpensSinceMonth and CompetitionOpensSinceYear 
+#Create a new variable CompetitionStart in date format combining
+#CompetitionOpensSinceMonth and CompetitionOpensSinceYear
 
 day <- 15
 rossman.store$CompetetionStart <- str_c(rossman.store$CompetitionOpenSinceYear,"-",rossman.store$CompetitionOpenSinceMonth,"-",day)
 rossman.store$CompetetionStart <- parse_date_time(rossman.store$Date, "Y-m-d", tz = "America/New_York")
 
-#Create a new variable PromoSinceDate in date format combining 
+#Create a new variable PromoSinceDate in date format combining
 #Promo2SinceYear and Promo2SinceWeek
 rossman.store$PromoSinceDate <- as.Date(paste(rossman.store$Promo2SinceYear,rossman.store$Promo2SinceWeek, 1, sep="-"), "%Y-%U-%u")
 
 #Remove unusable variables holding values of month and year
 rossman.store[,c(5,6,8,9)] <- NULL
 
+# Merge store data into the sales data
+
+merged.rossman.train <-
+  rossman.train %>%
+  left_join(rossman.store, by = 'Store')
+
+# Summary of the merged data
+
+summary(merged.rossman.train)
+
+# Transform the variables into the right format
+
+merged.rossman.train$DayOfWeek <-
+  as_factor(as.character(
+    merged.rossman.train$DayOfWeek))
+
+merged.rossman.train <-
+  merged.rossman.train %>%
+  mutate(DayOfWeek = fct_recode(DayOfWeek,
+                                "Monday" = "1" ,
+                                "Tuesday" = "2" ,
+                                "Wednesday" = "3" ,
+                                "Thursday" = "4" ,
+                                "Friday" = "5" ,
+                                "Saturday" = "6" ,
+                                "Sunday" = "7" ))
+
+merged.rossman.train %>%
+  group_by(StoreType) %>%
+  summarize(avgSalePerCustomer = round(mean(Sales/Customers, na.rm = T), 2),
+            avgSales = mean(Sales),
+            avgCustomers = mean(Customers))
+
+# Plot the average sales vs day of week
+
+merged.rossman.train %>%
+  group_by(DayOfWeek, StoreType) %>%
+  summarize(AvgSales = mean(Sales)) %>%
+  ggplot() +
+    geom_histogram(mapping = aes(x = DayOfWeek,
+                                 y = AvgSales,
+                                 fill = StoreType),
+                   stat = "identity")
