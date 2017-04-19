c2e047461cb4613ffc60046c6000158db551a08f
Sahil Thapar <sahilthapar1992@gmail.com>
2017-04-19 15:38:33 -0400

    Code formatting changes. Courtesy: Nitisha

--- scripts/time-series-exploration.R
+++ scripts/time-series-exploration.R
@@ -1,19 +1,23 @@
-library(lubridate) # Handling dates
-library(stringr) # Handling strings
-library(readr) # Reading csv files
-library(tidyr) # Data manipulation
-library(dplyr) # Data manipulation
-library(ggplot2) # Plots
-library(forcats) # Factors
-library(knitr) # Clean tables
-library(stats)
-library(ggfortify)
-library(scales)
-library(forecast)
-library(tibble)
+# Time series exploration and forecasting
 
-## Note setup:
-# Add your data folder into this directory and
+## Load libraries
+{
+  library(lubridate) # Handling dates
+  library(stringr) # Handling strings
+  library(readr) # Reading csv files
+  library(tidyr) # Data manipulation
+  library(dplyr) # Data manipulation
+  library(ggplot2) # Plots
+  library(forcats) # Factors
+  library(knitr) # Clean tables
+  library(stats)
+  library(ggfortify)
+  library(scales)
+  library(forecast)
+  library(tibble)
+}
+
+## Read and prepare data
 read.prep.data <- function() {
   rossman.train <- read_csv(file = './data/train.csv',
                             col_types = 'icDiiiici')
@@ -49,60 +53,65 @@ read.prep.data <- function() {
   return(list(rossman.train, rossman.test, rossman.store))
 }
 
-rossman.data <- read.prep.data()
-rossman.train <- rossman.data[[1]]
-rossman.test <- rossman.data[[2]]
-rossman.store <- rossman.data[[3]]
-rm(rossman.data)
+{
+  rossman.data <- read.prep.data()
+  rossman.train <- rossman.data[[1]]
+  rossman.test <- rossman.data[[2]]
+  rossman.store <- rossman.data[[3]]
+  rm(rossman.data)
+}
 
-store.1 <-
+## Get data for a store, store 1
+{
+  store.1 <-
   rossman.train %>% 
   filter(Store == 1) 
+}
 
-# Exploration
+## Exploration
+{
+  store.1 %>%
+    select(Date, Sales, DayOfWeek) %>%
+    ggplot() +
+    geom_line(mapping = aes(x = Date,
+                            y = Sales),
+              color = "#3498db") +
+    geom_hline(mapping = aes(yintercept = mean(Sales)),
+               color = '#e74c3c',
+               show.legend = T) + 
+    ggtitle(label = "Store 20: Daily Sales")
+  
+  store.1 %>%
+    select(Date, Sales, DayOfWeek) %>%
+    filter(Date < "2013-02-01") %>%
+    ggplot() +
+    geom_line(mapping = aes(x = Date,
+                            y = Sales),
+              color = "#3498db") +
+    geom_hline(mapping = aes(yintercept = mean(Sales)),
+               color = '#e74c3c',
+               show.legend = T) + 
+    ggtitle(label = "Store 20: Daily Sales in January, 2013")
+  
+  store.1 %>%
+    group_by(DayOfWeek) %>%
+    summarize(Avg_Sales = mean(Sales)) %>%
+    ggplot() +
+    geom_bar(mapping = aes(x = DayOfWeek,
+                           y = Avg_Sales),
+             fill = "#3498db",
+             stat = "identity") +
+    ggtitle(label = "Store 20: Daily Sales in January, 2013")
+}
 
-store.1 %>%
-  select(Date, Sales, DayOfWeek) %>%
-  ggplot() +
-  geom_line(mapping = aes(x = Date,
-                          y = Sales),
-            color = "#3498db") +
-  geom_hline(mapping = aes(yintercept = mean(Sales)),
-             color = '#e74c3c',
-             show.legend = T) + 
-  ggtitle(label = "Store 20: Daily Sales")
-
-store.1 %>%
-  select(Date, Sales, DayOfWeek) %>%
-  filter(Date < "2013-02-01") %>%
-  ggplot() +
-  geom_line(mapping = aes(x = Date,
-                          y = Sales),
-            color = "#3498db") +
-  geom_hline(mapping = aes(yintercept = mean(Sales)),
-             color = '#e74c3c',
-             show.legend = T) + 
-  ggtitle(label = "Store 20: Daily Sales in January, 2013")
-
-store.1 %>%
-  group_by(DayOfWeek) %>%
-  summarize(Avg_Sales = mean(Sales)) %>%
-  ggplot() +
-  geom_bar(mapping = aes(x = DayOfWeek,
-                         y = Avg_Sales),
-           fill = "#3498db",
-           stat = "identity") +
-  ggtitle(label = "Store 20: Daily Sales in January, 2013")
-
-# Fitting the forecast model
-
+## Generic function for fitting model
 forecast_fit <- function(d, take.log = T, func = ets) {
   d <-
     d %>%
     mutate(Sales = ifelse(Sales > 0,
                           log(Sales),
                           0))
-    
+  
   Sales <- ts(d$Sales[1:(nrow(d) - 42)], frequency = 7)
   lambda <- BoxCox.lambda(Sales)
   tsclean(Sales, replace.missing = TRUE, lambda = lambda)
@@ -117,31 +126,30 @@ forecast_fit <- function(d, take.log = T, func = ets) {
   return(fit)
 }
 
-fit_arima <- forecast_fit(store.1, auto.arima)
-fit_ets <- forecast_fit(store.1)
-
-forecast_arima <- forecast(object = fit_arima, h = 42)
-forecast_ets <- forecast(object = fit_ets, h = 42)
-
-# Function to quick check predictions
-get_mse <- function(predicted, actual){
-  diff <- predicted - actual
-  a <- cbind(predicted = predicted,
-             actual = actual,
-             diff = diff)
-  a <- as_tibble(a)
-  View(a)
-  return(mean(diff^2))
+## Arima and ets models
+{  
+  fit_arima <- forecast_fit(store.1, auto.arima)
+  fit_ets <- forecast_fit(store.1)
+  
+  forecast_arima <- forecast(object = fit_arima, h = 42)
+  forecast_ets <- forecast(object = fit_ets, h = 42)
+  
+  # Function to quick check predictions
+  get_mse <- function(predicted, actual){
+    diff <- predicted - actual
+    a <- cbind(predicted = predicted,
+               actual = actual,
+               diff = diff)
+    a <- as_tibble(a)
+    View(a)
+    return(mean(diff^2))
+  }
+  
+  pred.arima <- round(forecast_arima$mean, 2)
+  pred.ets <- round(forecast_ets$mean, 2)
+  actual <- tail(store.1$Sales, 42)
+  actual <- ifelse(actual > 0, log(actual), 0)
+  
+  get_mse(pred.arima, actual)
+  get_mse(pred.ets, actual)
 }
-
-pred.arima <- round(forecast_arima$mean, 2)
-pred.ets <- round(forecast_ets$mean, 2)
-actual <- tail(store.1$Sales, 42)
-actual <- ifelse(actual > 0, log(actual), 0)
-
-get_mse(pred.arima, actual)
-get_mse(pred.ets, actual)
-
-## Seasonality = 7
-
-
